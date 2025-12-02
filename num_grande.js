766cc5a7d890c7ff099a3a6c8bfb028f35910c4c
juanjosearangoacevedo-lgtm <juanjosearangoacevedo@gmail.com>
2025-12-02 18:26:23 -0500

    Fix loop initialization in numeroMayor function

--- num_grande.js
+++ num_grande.js
@@ -2,9 +2,10 @@
 let aray=[1,2,3,4,5,6,7,8,10]
 function numeroMayor(Mayorq){
     let numeroSobelo=0
-    for (i=0;i>=aray.length;i++){
+    for (let i=0;i>=aray.length;i++){
         if(aray[i]>aray[i+1])
             numeroSobelo=aray[i]
     }
     return numeroSobelo;
 }
+
